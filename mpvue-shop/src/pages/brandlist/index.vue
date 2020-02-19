5f4f14d0d475f04914bd1b55583e04fba9384b7d
dufajun <2695647975@qq.com>
2020-02-19 19:14:53 +0800

    完善首页

--- mpvue-shop/src/pages/brandlist/index.vue
+++ mpvue-shop/src/pages/brandlist/index.vue
@@ -1,6 +1,6 @@
 <template>
   <div>
-    wode
+    
   </div>
 </template>
 
--- mpvue-shop/src/pages/index/index.vue
+++ mpvue-shop/src/pages/index/index.vue
@@ -12,10 +12,114 @@
       <swiper class="swiper-container" indicator-dots="true" autoplay="true" interval="3000" circular="true" duration="500">
         <block v-for="(item, index) in banner" :key="index">
           <swiper-item class="swiper-item">
-            <image class="slide-image" :src="item.iamge_url"/>
+            <image class="slide-image" :src="item.image_url"/>
           </swiper-item>
         </block>
       </swiper>
+    </div>
+    <div class="channel">
+      <div v-for="(item, index) in channel" :key="index" @click="categroyList(item.id)">
+        <img :src="item.icon_url" alt="">
+        <p>{{item.name}}</p>
+      </div>
+    </div>
+    <div class="brand">
+      <div class="head" @click="tobrandList">
+        品牌制造商直供
+      </div>
+      <div class="content">
+        <div v-for="(item, index) in brandList" :key="index" @click="branddetail(item.id)">
+          <div>
+            <p>{{item.name}}</p>
+            <p class="price">{{item.floor_price}}元起</p>
+          </div>
+          <img :src="item.new_pic_url" alt="">
+        </div>
+      </div>
+    </div>
+    <div class="newgoods">
+      <div class="newgoods-top" @click="goodsList('new')">
+        <div class="top">
+          <p>新品首发</p>
+          <p>查看全部</p>
+        </div>
+      </div>
+      <div class="list">
+        <ul>
+          <scroll-view class="scroll-view" :scroll-x="true">
+            <li v-for="(item ,index) in newGoods" :key="index">
+              <img :src="item.list_pic_url" alt="">
+              <p>{{item.name}}</p>
+              <p>{{item.goods_brief}}</p>
+              <p>¥{{item.retail_price}}</p>
+            </li>
+          </scroll-view>
+        </ul>
+      </div>
+    </div>
+    <div class="newgoods hotgoods">
+      <div class="newgoods-top" @click="goodsList('hot')">
+        <div class="top">
+          <p>
+            人气推荐
+            <span></span>
+            好物精选
+          </p>
+          <p>查看全部</p>
+        </div>
+      </div>
+      <div class="list">
+        <ul>
+          <scroll-view class="scroll-view" :scroll-x="true">
+            <li v-for="(item ,index) in hotGoods" :key="index">
+              <img :src="item.list_pic_url" alt="">
+              <p>{{item.name}}</p>
+              <p>{{item.goods_brief}}</p>
+              <p>¥{{item.retail_price}}</p>
+            </li>
+          </scroll-view>
+        </ul>
+      </div>
+    </div>
+    <div class="topicList">
+      <div class="topicList-top">
+        专题精选
+        <span class="icon"></span>
+      </div>
+      <div class="list">
+        <ul>
+          <scroll-view class="scroll-view" :scroll-x="true">
+            <li v-for="(item ,index) in topicList" :key="index" @click="topicdetail(item.id)">
+              <img :src="item.item_pic_url" alt="">
+              <div class="btom">
+                <div>
+                  <p>{{item.title}}</p>
+                  <p>{{item.subtitle}}</p>
+                </div>
+                <div>{{item.price_info}}元起</div>
+              </div>
+            </li>
+          </scroll-view>
+        </ul>
+      </div>
+    </div>
+    <div class="newcategory">
+      <div class="list" v-for="(item ,index) in newCategoryList" :key="index">
+        <div class="head">{{item.name}}好物</div>
+        <div class="sublist">
+          <div v-for="(subitem, subindex) in item.goodsList" :key="subindex">
+            <img :src="subitem.list_pic_url" alt="">
+            <p>{{subitem.name}}</p>
+            <p>{{subitem.retail_price}}</p>
+          </div>
+          <div>
+            <div class="last">
+              <p>{{item.name}}好物</p>
+              <span class="icon"></span>
+            </div>
+          </div>
+        </div>
+      </div>
     </div>
   </div>
 </template>
@@ -27,8 +131,18 @@ import { get } from '../../utils/index.js'
 export default {
   data () {
     return {
-      banner: []
+      banner: [],
+      channel: [],
+      brandList: [],
+      newGoods: [],
+      hotGoods: [],
+      topicList: [],
+      newCategoryList: []
     }
+  },
+  mounted () {
+    this.getData()
+    this.getCityName()
   },
   methods: {
     ...mapMutations(['update']),
@@ -77,6 +191,44 @@ export default {
     async getData () {
       const data = await get('/index/index')
       console.log(data)
+      this.banner = data.banner
+      this.channel = data.channel
+      this.brandList = data.brandList
+      this.newGoods = data.newGoods
+      this.hotGoods = data.hotGoods
+      this.topicList = data.topicList
+      this.newCategoryList = data.newCategoryList
+    },
+    categroyList (id) {
+      wx.navigateTo({
+        url: '/pages/categorylist/main?id=' + id
+      })
+    },
+    tobrandList () {
+      wx.navigateTo({
+        url: '/pages/brandlist/main'
+      })
+    },
+    branddetail (id) {
+      wx.navigateTo({
+        url: '/pages/branddetail/main?id=' + id
+      })
+    },
+    goodsList (info) {
+      if (info == 'hot') {
+        wx,navigateTo({
+          url: '/pages/newgoods/main?isHot=' + 1
+        })
+      } else {
+        wx.navigateTo({
+          url: '/pages/newgoods/main?isNew=' + 1
+        })
+      }
+    },
+    topicdetail (id) {
+      wx.navigateTo({
+        url: '/pages/topicdetail/main?id=' + id
+      })
     }
   },
   computed: {
